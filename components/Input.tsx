1c2ff14bdffa8d0d90459b89c59b5246c59e4c61
ommungelwar <ommungelwar123@gmail.com>
2025-01-08 20:15:42 +0530

    components\Input.tsx made the changes

--- components/Input.tsx
+++ components/Input.tsx
@@ -12,4 +12,11 @@ export const Input = ({label, placeholder, onChange, type = "text"}: {
         </div>
         <input className="border rounded px-4 py-2 w-full border-black" type={type} placeholder={placeholder} onChange={onChange} />
     </div>
+
+
+return <div> 
+    <div className="text-sm pb-1 pt-2">
+
+    </div>
+</div>
 }
